31b4129db28e23a12ec6f778f9546a09c463a81b
Max <mushbit@protonmail.com>
2023-01-25 11:50:15 +0100

    Second and last commit. Works like a charm.

---
+++ caesar_cipher.rb
@@ -0,0 +1,21 @@
+def caesar_cipher(input_string, rotations)
+
+    alphabet = ("a".."z").to_a
+    alphabet_capital = ("A".."Z").to_a
+    rotated_alphabet = alphabet.rotate(rotations)
+    rotated_alphabet_capital = alphabet_capital.rotate(rotations)
+    
+    input_string.split("").map do |character|
+        if character.match(/[a-z]/)
+            character = rotated_alphabet[alphabet.index(character)]
+        elsif character.match(/[A-Z]/)
+            character = rotated_alphabet_capital[alphabet_capital.index(character)]
+        else
+            character
+        end
+    end.join("")
+    
+
+end
+
+p caesar_cipher("Could I please have some GOD DAMN COLAAAAAA!!!", 5)
